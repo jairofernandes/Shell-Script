bd3ad60846519b77256e934eaf5e910236c0e442
Jairo Fernandes <jairofernandes@gmail.com>
2020-10-07 14:33:37 -0300

    Update mailSync_ispconfig.sh

--- mailSync_ispconfig.sh
+++ mailSync_ispconfig.sh
@@ -4,13 +4,13 @@ SOURCE_SERVER='IpDoServidorDeOrigem'
 DESTINATION_SERVER='IpDoServidorDeDestino'
 PASSWORD="h2jxkLAr@8ZN"
 DEFAULT_PASSWORD='$6$rounds=5000$159a3f62ed8cced5$.bkdRSlQqS58BEqzhuhC16yxmyIkaLwsnZj3q9Y2No0hHGBQToU3ph9hOz8R1L7bvY2uFJ9A0a9AwAgH5FvjS/'
-#DEFAULT_PASSWORD='$H4lzeITIguNFdxKwDFPweDmdsXfmlFWnGWGgrx.VHok9Ee/BSoL6Iime6Lr7EG6yC.JjQ9RAONgC4sPY/RlvF0'
 CONFIGFILE=$1
 COMMAND=$(which imapsync)
 
 for DADOS in $(cat $CONFIGFILE); do
 
         USERNAME=$(echo $DADOS | awk -F ";" '{print $1}')
+ 
         ORIGIN_PASSWORD=$(echo $DADOS | awk -F ";" '{print $2}')
 
         mysql --defaults-group-suffix=remoto -e "UPDATE dbispconfig.mail_user SET password='$DEFAULT_PASSWORD' WHERE login='$USERNAME';"
