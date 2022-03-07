cadcb623872794e068ae5b71ea241dee471f2ae0
Jairo Fernandes <jairofernandes@gmail.com>
2022-03-07 16:34:08 -0300

    Create upunifi.sh

---
+++ upunifi.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+VERSION=$1
+TEMPDIR='/tmp/unifi'
+DATA=$(date +"%d%m%Y")
+
+wget -c https://dl.ui.com/unifi/$VERSION/UniFi.unix.zip -O $TEMPDIR/UniFi.unix.$VERSION.zip
+
+systemctl stop unifi
+
+tar -C /opt/UniFi -zcpvf backup_$DATA.tar.gz data
+
+unzip -qo $TEMPDIR/UniFi.unix.$VERSION.zip -d /opt
+
+chown -R ubnt.ubnt /opt/UniFi
+
+tar -C /opt/UniFi -xzpvf backup_$DATA.tar.gz
+
+systemctl start unifi
